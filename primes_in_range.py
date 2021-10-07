6b997817eeea539b4a4322f24dcd723029382217
lightningC00kie <kareemhamed24@gmail.com>
2021-10-08 01:49:33 +0200

    first commit

---
+++ primes_in_range.py
@@ -0,0 +1,39 @@
+import time
+
+A = int(input())
+B = int(input())
+start_time = time.time()
+
+smallest = B
+largest = counter = percentage = 0
+
+for i in range(A, B+1):
+    n = 2
+    is_prime = True
+    while n * n <= i:
+        if i % n == 0:
+            is_prime = False
+            break
+        n += 1
+    
+    if is_prime:
+        counter += 1
+
+    if i < smallest and is_prime:
+        smallest = i
+    
+    if i > largest and is_prime:
+        largest = i
+
+if counter == 0:
+    smallest = largest = None
+else:
+    percentage = (counter/((B-A)+1)) * 100
+    
+
+print('Number of primes: ', counter)
+print('Smallest prime: ', smallest)
+print('Largest prime: ', largest)
+print('Percentage of numbers that are prime: ', f'{percentage:.1f}')
+
+print("--- %s seconds ---" % (time.time() - start_time))
