b13c51c065fc50ff2930f2c999aaf05d15d44f71
lightningC00kie <kareemhamed24@gmail.com>
2021-10-16 22:42:15 +0200

    formatted dcoument

--- prime_factorization_II.py
+++ prime_factorization_II.py
@@ -1,17 +1,17 @@
 n = temp = int(input())
 factors = ''
-i=2
+i = 2
 prod = 1
 repeat = True
 counter = 0
 counter2 = 0
 while prod != n:
-# checking if i is a prime number in each iteration of the outer while loop
+    # checking if i is a prime number in each iteration of the outer while loop if i has changed since the last loop
     if repeat:
-        
+
         prime = True
-        j = 2           
-        
+        j = 2
+
         while j * j <= i:
             counter2 += 1
             if i % j == 0:
@@ -23,7 +23,7 @@ while prod != n:
 
 # if temp is divisible by i and i is prime then we continue to divide the new value of temp by i and add the number of times
 # we divide by i to a counter variable until eventually temp will not be divisible by i
-    if temp % i == 0 and prime==True:
+    if temp % i == 0 and prime == True:
         repeat = False
         counter += 1
         temp = temp // i
