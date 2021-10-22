908874b97153e12a0d456b2d0d61910c0de75b36
lightningC00kie <kareemhamed24@gmail.com>
2021-10-22 22:50:09 +0200

    goldbach

--- goldbach_sums.py
+++ goldbach_sums.py
@@ -3,30 +3,30 @@ l = [int(i) for i in n.split()]
 count = [0] * len(l)
 largest = max(l)
 
+prime = [True] * (largest+1)
+j = 2
 
+while j * j <= largest:
+    if prime[j]:
+        for m in range(j*2, largest+1, j):
+            prime[m] = False
+    j += 1
 
 for i in range(len(l)):
     if l[i] % 2 != 0 and l[i] > 3:
         a = 2
         b = l[i] - 2
+        if prime[b]:
+            count[i] += 1
+            continue
     else:
         for ii in range(2, (l[i]//2)+1):
 
             a = ii
             b = l[i] - ii
+            if prime[a] and prime[b]:
+                count[i] += 1
 
-    prime = [True] * (b+1)
-    j = 2
-
-    while j * j <= b:
-        if prime[j]:
-            for m in range(j*2, b+1, j):
-                prime[m] = False
-        j += 1
-
-    if prime[a] and prime[b]:
-        count[i] += 1
-        
 c = [str(i) for i in count]
 
 c = ' '.join(c)
